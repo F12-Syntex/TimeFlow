02e96ffc339191c205fef4b77858e82b49f03e18
SciXYolk <cameleongaming07@gmail.com>
2023-12-19 14:42:30 +0000

    feat: updated button message

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Electron + Vite + React</h1>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          rahart likes boys {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR

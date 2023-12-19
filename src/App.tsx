42ac50cf3af7be8c370437c659c27692d41a02c4
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2023-12-19 14:47:35 +0000

    feat: updates button message

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Electron + Vite + React</h1>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
-          rahart likes boys {count}
+          rahart likes boys {count} alot
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR

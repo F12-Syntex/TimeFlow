03caf44253e8c68b4edeac57dcb40c18a197f503
Desgarrosa <djrruano@gmail.com>
2024-01-03 23:55:42 +0000

    matrix for calendar made?

--- src/components/update/Calendar/calendar.tsx
+++ src/components/update/Calendar/calendar.tsx
@@ -1,12 +1,23 @@
 import dayjs from 'dayjs' //import dayjs
 
 export function getMonth(month = dayjs().month()){ 
+
     const year = dayjs().year()
-    const firstDayOfMonth = dayjs(new Date(year, month, 1)).day()
+    const firstDayOfMonth = dayjs(new Date(year, month, 1)).day() 
+    let curMonthCounter = 0 - firstDayOfMonth;
 
     const dayMatrix = new Array(5).fill([]).map(()=>{
-        new Array(7).fill(null)
+        return new Array(7).fill(null).map(()=>{
+            curMonthCounter++; 
+            return dayjs(new Date(year, month, curMonthCounter))
+        })
     })
+    return dayMatrix;
 
+    
+}
+function App(){
 
 }
+
+export default App

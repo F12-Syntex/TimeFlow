595d71b6fae7e0c5a167e542a9017156fdaaf659
snehnilesh <snehnilesh123@hotmail.com>
2024-01-04 00:06:13 +0000

    feat: base calendar updates, install dayjs

--- src/components/update/Calendar/calendar.tsx
+++ src/components/update/Calendar/calendar.tsx
@@ -1,23 +1,32 @@
-import dayjs from 'dayjs' //import dayjs
+import dayjs from "dayjs"; //import dayjs
+import React from "react";
 
-export function getMonth(month = dayjs().month()){ 
-
-    const year = dayjs().year()
-    const firstDayOfMonth = dayjs(new Date(year, month, 1)).day() 
+function App() {
+  function getMonth(month = dayjs().month()) {
+    const year = dayjs().year();
+    const firstDayOfMonth = dayjs(new Date(year, month, 1)).day();
     let curMonthCounter = 0 - firstDayOfMonth;
 
-    const dayMatrix = new Array(5).fill([]).map(()=>{
-        return new Array(7).fill(null).map(()=>{
-            curMonthCounter++; 
-            return dayjs(new Date(year, month, curMonthCounter))
-        })
-    })
+    const dayMatrix = new Array(5).fill([]).map(() => {
+      return new Array(7).fill(null).map(() => {
+        curMonthCounter++;
+        return dayjs(new Date(year, month, curMonthCounter));
+      });
+    });
     return dayMatrix;
+  }
 
-    
+  console.table(getMonth(3));
+
+  return (
+    <React.Fragment>
+        <div className="h-screen flex flex-columns">
+            <div className="flex flex-1">
+
+            </div>
+        </div>
+    </React.Fragment>
+  );
 }
-function App(){
 
-}
-
-export default App
+export default App;
